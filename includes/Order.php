ebe4736add3b1a985e51a61f99bd40f13b5f8511
Jordan Blackmon <disasterisk@users.noreply.github.com>
2017-02-08 12:04:34 -0800

    Update Order.php
    Syntax fix + tax stuff by adoboeater

--- includes/Order.php
+++ includes/Order.php
@@ -5,23 +5,29 @@ include 'Items.php';
 echo '<table><tr><th>Food</th><th>Price</th><th>Description</th><th>Amount Ordered</th>';
 foreach ($menu as $menuItem) {
     echo '<tr>';
-    echo '<td>' . $menuItem->{Name} . '</td>';
-    echo '<td>' . $menuItem->{Price} . '</td>';
-    echo '<td>' . $menuItem->{Description} . '</td>';
-    echo '<td>' . $_POST[$menuItem->{Name}] . '</td>';
+    echo '<td>' . $menuItem->Name . '</td>';
+    echo '<td>' . $menuItem->Price . '</td>';
+    echo '<td>' . $menuItem->Description . '</td>';
+    echo '<td>' . $_POST[$menuItem->Name] . '</td>';
     echo '</tr>';
 }
 
-$total = 0;
+$subTotal = 0;
+$taxRate;
+$tax = 0;
+$total;
 
 foreach ($menu as $menuItem) {
-$total = $total + ($menuItem->{Price} * $_POST[$menuItem->{Name}]);
+$subTotal = $subTotal + ($menuItem->Price * $_POST[$menuItem->Name]);
+$taxRate = 9.5 / 100;
+$tax = $taxRate * $subTotal;
+$total = $tax + $subTotal;
 
 }
 
 
 echo '</table>';
 
-echo 'Your total is $' . $total .'.';
-
-
+echo '<p>Your subtotal: $' . $subTotal . '</p>';
+echo '<p>Tax rate of 9%: ' . $tax . '</p>';
+echo '<p>Your total is: ' . $total . '</p>';
