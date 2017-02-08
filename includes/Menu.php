9f48baf5eaa71a7510fa59a203be61f603293a73
Jordan Blackmon <disasterisk@users.noreply.github.com>
2017-02-08 12:01:28 -0800

    Update Menu.php
    Syntax fix

--- includes/Menu.php
+++ includes/Menu.php
@@ -7,12 +7,10 @@ echo '<table><tr><th>Food</th><th>Price</th><th>Description</th><th>Amount</th>'
 
 foreach ($menu as $menuItem) {
     echo '<tr>';
-    echo '<td>'. $menuItem -> {Name} . '</td>';
-    echo '<td>' . $menuItem -> {Price} . '</td>';
-    echo '<td>' . $menuItem -> {Description} . '</td>';
-    echo '<td> <input type="number"  value="0" name="' . $menuItem -> {Name} . '" /></td>';
+    echo '<td>'. $menuItem->Name . '</td>';
+    echo '<td>' . $menuItem->Price . '</td>';
+    echo '<td>' . $menuItem ->Description . '</td>';
+    echo '<td> <input type="number"  value="0" name="' . $menuItem->Name . '" /></td>';
     echo '</tr>';
 }
 echo '</table><input type="submit" name="submit" value="Submit" /></form>';
-
-
